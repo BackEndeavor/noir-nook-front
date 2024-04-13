e20eaf05e3c742eec7a062452921b95fe74f1c9f
bivashy <botyrbojey@gmail.com>
2024-04-14 04:28:22 +0500

    Add basic components

---
+++ src/lib/ui/Card.svelte
@@ -0,0 +1,50 @@
+<script>
+    export let loading = true;
+    export let content = {
+        image: '',
+        title: '',
+        subTitle: '',
+        mainTag: '',
+        tags: []
+    };
+</script>
+
+<div class="card w-80 bg-base-200 border border-base-300 border-solid shadow-xl {$$props.class || ''}">
+    {#if loading}
+        <figure>
+            <div class="skeleton w-full h-32 rounded-b-none"></div>
+        </figure>
+        <div class="card-body">
+            <h2 class="card-title">
+                <div class="skeleton w-24 h-5"></div>
+                <div class="skeleton w-16 h-6"></div>
+            </h2>
+            <div class="skeleton h-4"></div>
+            <div class="card-actions justify-end">
+                <div class="skeleton w-16 h-5"></div>
+                <div class="skeleton w-16 h-5"></div>
+            </div>
+        </div>
+    {:else}
+        <figure>
+            <!-- Optionally add an <img> or other content element here -->
+            <img src="{content.image}" alt="{content.subTitle}" class="h-48 w-full"/>
+        </figure>
+        <div class="card-body">
+            <h2 class="card-title">
+                <div class="text-start m-0 p-0 box-border">
+                    {content.title}
+                </div>
+                {#if content.mainTag}
+                    <div class="badge badge-secondary line-clamp-1">{content.mainTag}</div>
+                {/if}
+            </h2>
+            <p>{content.subTitle}</p>
+            <div class="card-actions justify-end">
+                {#each content.tags as tag}
+                    <div class="badge badge-outline">{tag}</div>
+                {/each}
+            </div>
+        </div>
+    {/if}
+</div>
---
+++ src/lib/ui/theme/ThemeButton.svelte
@@ -0,0 +1,17 @@
+<script>
+    import Sun from "$lib/icon/Sun.svelte";
+    import Moon from "$lib/icon/Moon.svelte";
+</script>
+
+<label class="swap swap-rotate btn btn-ghost">
+    <!-- this hidden checkbox controls the state -->
+    <input type="checkbox" class="theme-controller" value="light"/>
+
+    <div class="swap-off fill-current w-10 h-10">
+        <Sun/>
+    </div>
+    <div class="swap-on fill-current w-10 h-10">
+        <Moon/>
+    </div>
+
+</label>
