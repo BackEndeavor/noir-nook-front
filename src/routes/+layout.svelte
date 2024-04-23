ffa6b07a43363a2d6a1af0d242f90b87f50915f1
bivashy <botyrbojey@gmail.com>
2024-04-24 02:43:46 +0500

    Add SignIn and SignOut buttons with profile image

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,18 @@
 <script lang="ts">
     import '../app.css';
+    import 'iconify-icon';
     import Logo from "$lib/icon/Logo.svelte";
     import ThemeButton from "$lib/ui/theme/ThemeButton.svelte";
+    import {SignIn, SignOut} from "@auth/sveltekit/components";
+    import type {PageData} from './$types';
 
     let searchInput = '';
+
     function search(): void {
         console.log(`Searching "${searchInput}"...`)
     }
+
+    export let data: PageData;
 </script>
 
 <div class="navbar bg-base-300">
@@ -18,10 +24,33 @@
     </div>
     <div class="flex-none gap-2">
         <!-- TODO: Make functional search bar -->
-        <form class="form-control" on:submit|preventDefault={() => search()} >
+        <form class="form-control" on:submit|preventDefault={() => search()}>
             <input bind:value={searchInput} type="text" placeholder="Search" class="input input-bordered w-24 md:w-auto"/>
         </form>
         <ThemeButton/>
+        <div class="flex">
+            {#if data.session === null}
+                <SignIn signInPage="signin" className="bg-base-100 rounded-lg">
+                    <button class="btn btn-ghost" slot="submitButton">
+                        Sign in
+                    </button>
+                </SignIn>
+            {:else}
+                <div class="dropdown dropdown-bottom dropdown-end">
+                    <div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar self-center">
+                        <div class="w-10 rounded-full">
+                            <img alt={data?.session?.user?.name} src={data?.session?.user?.image}/>
+                        </div>
+                    </div>
+                    <ul tabindex="0" class="dropdown-content z-[1] menu p-2  border border-neutral bg-base-200 rounded-box w-48">
+                        <li><a>Profile</a></li>
+                        <li>
+                            <SignOut signOutPage="signout"></SignOut>
+                        </li>
+                    </ul>
+                </div>
+            {/if}
+        </div>
     </div>
 </div>
 <slot/>
