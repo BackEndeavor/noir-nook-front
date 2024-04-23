ab606b50fa1f1868325acda1e3731054aa2492c2
bivashy <botyrbojey@gmail.com>
2024-04-24 02:45:58 +0500

    Use components instead of daisy-ui classes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,17 @@
 <script lang="ts">
     import Card from "$lib/ui/card/Card.svelte";
     import CardBody from "$lib/ui/card/CardBody.svelte";
+    import Divider from "$lib/ui/Divider.svelte";
+    import Join from "$lib/ui/layout/Join.svelte";
+    import CardImage from "$lib/ui/card/CardImage.svelte";
+    import CardTitle from "$lib/ui/card/CardTitle.svelte";
+
+    export let data;
 </script>
 
 <div class="flex flex-col w-full">
     <Card class="self-center my-4"></Card>
-    <div class="divider w-4 self-center"></div>
+    <Divider class="w-4 self-center"></Divider>
     <Card class="self-center w-96 my-4">
         <CardBody class="items-center">
             <p class="font-light">Advertisement</p>
@@ -15,7 +21,7 @@
     </Card>
     <div class="flex flex-col self-center my-16">
         <p class="font-bold">Latest Post</p>
-        <div class="divider"></div>
+        <Divider></Divider>
         <div class="grid grid-cols-3 gap-4 self-center">
             <Card class="col-span-3 lg:col-span-1"></Card>
             <Card class="col-span-3 lg:col-span-1"></Card>
@@ -25,19 +31,19 @@
             <Card class="col-span-3 lg:col-span-1"></Card>
         </div>
     </div>
-    <div class="join self-center">
+    <Join class="self-center">
         <button class="join-item btn">1</button>
         <button class="join-item btn">2</button>
         <button class="join-item btn btn-disabled">...</button>
         <button class="join-item btn">99</button>
         <button class="join-item btn">100</button>
-    </div>
-    <div class="divider w-4 self-center my-16"></div>
-    <div class="card w-72 self-center bg-base-200 shadow-xl">
-        <div class="card-body items-center">
+    </Join>
+    <Divider class="w-4 self-center my-16"></Divider>
+    <Card class="w-72 self-center">
+        <CardBody class="items-center">
             <p class="font-light">Advertisement</p>
             <h2 class="card-title">You can place ads!</h2>
             <p class="font-light">400x200</p>
-        </div>
-    </div>
+        </CardBody>
+    </Card>
 </div>
