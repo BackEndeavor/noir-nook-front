bd80180a1b3732fa8a21300aab281a1fb9529c41
bivashy <botyrbojey@gmail.com>
2024-04-24 20:18:17 +0500

    Refresh access token after expiration

--- src/auth.ts
+++ src/auth.ts
@@ -2,7 +2,9 @@ import {SvelteKitAuth} from "@auth/sveltekit"
 import GitHub from "@auth/sveltekit/providers/github"
 import Google from "@auth/sveltekit/providers/google"
 
-async function authenticate(provider: string, access_token: string) {
+const AVAILABLE_PROVIDERS = ['google', 'github']
+
+async function authenticate(provider: string, access_token: string | undefined) {
     return await fetch(`http://localhost:8000/api/auth/${provider}/`, {
         method: 'POST',
         headers: {
@@ -14,21 +16,55 @@ async function authenticate(provider: string, access_token: string) {
     })
 }
 
+async function refreshAccessToken(refreshToken: string) {
+    return await fetch('http://localhost:8000/api/auth/token/refresh/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': `application/json`
+        },
+        body: JSON.stringify({
+            refresh: refreshToken
+        })
+    })
+}
+
+export type CreateTokenResponse = {
+    access: string,
+    refresh: string,
+    access_expiration: string,
+    refresh_expiration: string
+}
+
+export type RefreshTokenResponse = {
+    access: string,
+    access_expiration: string
+}
+
 export const {handle, signIn, signOut} = SvelteKitAuth({
     providers: [Google, GitHub],
     callbacks: {
         async session({session, token}) {
-            return {...session, access_token: token.access_token}
+            return {...session, access_token: token.access_token, error: token.error}
         }, async jwt({token, account}) {
-            if (!account || !account.access_token)
+            const accessExpirationDate = token.access_expiration as number | undefined
+            const refreshExpirationDate = token.refresh_expiration as number | undefined
+            const refreshToken = token.refresh_token as string | undefined
+            if (account && AVAILABLE_PROVIDERS.includes(account.provider)) {
+                const response = await authenticate(account.provider, account.access_token).then(data => data.json()).then(data => data as CreateTokenResponse)
+                token.access_token = response.access
+                token.refresh_token = response.refresh
+                token.access_expiration = new Date(response.access_expiration).getTime()
+                token.refresh_expiration = new Date(response.refresh_expiration).getTime()
+            } else if (accessExpirationDate && Date.now() < accessExpirationDate) {
+                return token;
+            } else if (refreshExpirationDate && Date.now() < refreshExpirationDate) {
+                // TODO: Handle refresh token expiration through throwing error
                 return token
-            if (account.provider === 'google') {
-                const response = authenticate(account.provider, account.access_token).then(data => data.json())
+            } else if (refreshToken) {
+                const response = await refreshAccessToken(refreshToken).then(data => data.json()).then(data => data as RefreshTokenResponse)
                 token.access_token = response.access
-            }
-            if (account.provider === 'github') {
-                const response = authenticate(account.provider, account.access_token).then(data => data.json())
-                token.access_token = response.access
+                token.access_expiration = new Date(response.access_expiration).getTime()
+                return token;
             }
             return token;
         },
