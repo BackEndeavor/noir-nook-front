6958221335cfa45493e17671d2cdea91a27a4de7
bivashy <botyrbojey@gmail.com>
2024-04-29 21:42:40 +0500

    Add new UI elements

---
+++ src/lib/ui/feedback/Tooltip.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    export let data: string;
+</script>
+
+<div class="tooltip {$$props.class || ''}" data-tip={data}>
+    <slot></slot>
+</div>
---
+++ src/lib/ui/util/ScrollToTop.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    export let scrollHeightTrigger = 10;
+    let button: HTMLButtonElement;
+
+    function scrollToTop() {
+        window.scrollTo({top: 0, behavior: 'smooth'});
+    }
+
+    function handleScroll() {
+        const shown = document.body.scrollTop > scrollHeightTrigger || document.documentElement.scrollTop > scrollHeightTrigger;
+        button.style.opacity = shown ? '1' : '0';
+    }
+</script>
+
+<svelte:window on:scroll={handleScroll}/>
+
+<div class="fixed right-0 bottom-0 m-8">
+    <button class="btn btn-accent btn-circle transition duration-300 {$$props.class || ''}" on:click={() => scrollToTop()} bind:this={button}>
+        <iconify-icon class="neutral" icon="mdi:arrow-top-bold" width="32" height="32"></iconify-icon>
+    </button>
+</div>
